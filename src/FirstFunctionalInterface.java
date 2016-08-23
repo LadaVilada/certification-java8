49f31739fc89627bc26866dcf055ed7f974bc352
ladavilada <bobina.vlada@gmail.com>
2016-08-23 12:23:03 +0300

    First commit

---
+++ src/FirstFunctionalInterface.java
@@ -0,0 +1,16 @@
+/**
+ * Created by vlada on 23.08.16.
+ */
+@FunctionalInterface
+public interface FirstFunctionalInterface {
+    //abstract method could be only one
+     void doWork();
+    //abstract methods from Object class
+
+     String toString();
+     boolean equals(Object o);
+
+    //second abstract method, cause error!
+//    public boolean isFunctional();
+
+}
