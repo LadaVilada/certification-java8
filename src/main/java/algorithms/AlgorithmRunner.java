2591b12f15fcbc8c9592e82aa3f6db276212c9c5
Vlada Bobina <vlada.bobina@t-systems.ru>
2017-11-08 18:22:15 +0300

    factorial algorithm

--- src/main/java/algorithms/AlgorithmRunner.java
+++ src/main/java/algorithms/AlgorithmRunner.java
@@ -1,4 +1,14 @@
 package algorithms;
 
+import algorithms.impl.FactorialAlgorithm;
+
 public class AlgorithmRunner {
+
+    private static FactorialAlgorithm factorialAlgorithm = new FactorialAlgorithm();
+
+    public static void main(String[] args) {
+        System.out.println("5! = " + factorialAlgorithm.simple(5));
+        System.out.println("1! = " + factorialAlgorithm.simple(1));
+        System.out.println("10! = " + factorialAlgorithm.simple(10));
+    }
 }
--- src/main/java/algorithms/impl/FactorialAlgorithm.java
+++ src/main/java/algorithms/impl/FactorialAlgorithm.java
@@ -1,4 +1,29 @@
 package algorithms.impl;
 
+/**
+ * The type Factorial algorithm.
+ */
 public class FactorialAlgorithm {
+
+    /**
+     * Instantiates a new Factorial algorithm.
+     */
+    public FactorialAlgorithm() {
+    }
+
+    /**
+     * Native implementation.
+     * <p>
+     * N! = N * (N-1)!
+     *
+     * @param n the n
+     * @return int = n!
+     */
+    public int simple(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
